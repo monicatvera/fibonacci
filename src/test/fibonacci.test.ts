0074e8c988dd8e66a5fc646052545dddbac9f01d
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-10-01 18:37:26 +0100

    ✅ finish kate

--- src/test/fibonacci.test.ts
+++ src/test/fibonacci.test.ts
@@ -1,15 +1,15 @@
 import { fibonacci } from "../core/fibonacci";
 describe('The fibonacci sequence', () => {
     it('yields value zero to number zero', () => {
-      expect(fibonacci(0)).toBe(0);
-    });
-
-    it('yields value one to number one', () => {
+        expect(fibonacci(0)).toBe(0);
+      });
+    
+      it('yields value one to number one', () => {
         expect(fibonacci(1)).toBe(1);
-     });
-
-     it('is a series where the value for a number is the addition of the preceding two values', () => {
-        expect(fibonacci(2)).toBe(fibonacci(0) + fibonacci(1));
-        expect(fibonacci(3)).toBe(fibonacci(1) + fibonacci(2));
+      });
+    
+      it('is a series where the value for a number is the addition of the preceding two values', () => {
+        [2, 3, 4, 5].forEach((n) =>
+          expect(fibonacci(n)).toBe(fibonacci(n - 1) + fibonacci(n - 2)))
       });
   });
